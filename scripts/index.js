08a105ae87ea27beb86f5f6445c317ad267df17d
Alina <alinbrode@yandex.ru>
2024-03-12 12:59:16 +0300

    the first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,14 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function addCard(item, delCard) {
+  const template = document.querySelector('#card-template').content;
+  let htmlItem = template.querySelector('.places__item').cloneNode(true);
+  htmlItem.querySelector('.card__title').textContent=item.name;
+  htmlItem.querySelector('.card__image').src=item.link;
+  return htmlItem;
+}
+
+
+document.querySelector('.places__list').append(...initialCards.map(addCard));
