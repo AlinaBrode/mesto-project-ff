6d084c4e99bdfbc28f2b9d0708605ebe21a410c5
Alina <alinbrode@yandex.ru>
2024-04-24 17:43:01 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { addCard, likeCard, delCard } from "./card.js";
 import {
   openPopup,
   closePopup,
-  onClosePopup,
   onClosePopupEsc,
   onOverlayClose,
 } from "./modal.js";
@@ -24,10 +23,7 @@ const popupCaption = document.querySelector(".popup__caption");
 const placesList = document.querySelector(".places__list");
 
 function viewImage(event) {
-  event.stopPropagation();
-  openPopup(popupTypeImage, onOverlayClose, onClosePopupEsc);
-  document.addEventListener("click", onOverlayClose);
-  document.addEventListener("keydown", onClosePopupEsc);
+  openPopup(popupTypeImage);
 
   popupImage.src = event.target.src;
   popupImage.alt = event.target.alt;
@@ -38,11 +34,11 @@ placesList.append(
   ...initialCards.map((descr) => addCard(descr, delCard, likeCard, viewImage))
 );
 
-function handleFormSubmit(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = formEditProfile.name.value;
   profileDescription.textContent = formEditProfile.description.value;
-  closePopup(popupTypeEdit, onOverlayClose, onClosePopupEsc);
+  closePopup(popupTypeEdit);
 }
 
 function handleAddCard(evt) {
@@ -63,26 +59,33 @@ function handleAddCard(evt) {
   );
 
   placesList.prepend(newCard);
-  closePopup(popupTypeNewCard, onOverlayClose, onClosePopupEsc);
+  closePopup(popupTypeNewCard);
 }
 
-formEditProfile.addEventListener("submit", handleFormSubmit);
+formEditProfile.addEventListener("submit", handleProfileFormSubmit);
 formAddCard.addEventListener("submit", handleAddCard);
 
 function openDialogEditProfile(evt) {
   formEditProfile.name.value = profileTitle.textContent;
   formEditProfile.description.value = profileDescription.textContent;
 
-  evt.stopPropagation(); // prevents closing of popup by overlay-click logic
-
-  openPopup(popupTypeEdit, onOverlayClose, onClosePopupEsc);
+  openPopup(popupTypeEdit);
 }
 
 function openDialogNewCard(evt) {
-  evt.stopPropagation(); // prevents closing of popup by overlay-click logic
-
-  openPopup(popupTypeNewCard, onOverlayClose, onClosePopupEsc);
+  openPopup(popupTypeNewCard);
 }
 
 profileAddButton.addEventListener("click", openDialogNewCard);
 profileEditButton.addEventListener("click", openDialogEditProfile);
+
+// находим все крестики проекта по универсальному селектору
+const closeButtons = document.querySelectorAll(".popup__close");
+
+// с окончанием `s` нужно обязательно, так как много кнопок
+closeButtons.forEach((button) => {
+  // находим 1 раз ближайший к крестику попап
+  const popup = button.closest(".popup");
+  // устанавливаем обработчик закрытия на крестик
+  button.addEventListener("click", () => closePopup(popup));
+});
--- src/modal.js
+++ src/modal.js
@@ -1,21 +1,10 @@
-export function openPopup(popup, onOverlayClose, onClosePopupEsc) {
+export function openPopup(popup) {
   popup.classList.add("popup_is-opened");
   popup.addEventListener("click", onOverlayClose);
   document.addEventListener("keydown", onClosePopupEsc);
-
-  // находим все крестики проекта по универсальному селектору
-  const closeButtons = document.querySelectorAll(".popup__close");
-
-  // с окончанием `s` нужно обязательно, так как много кнопок
-  closeButtons.forEach((button) => {
-    // находим 1 раз ближайший к крестику попап
-    const popup = button.closest(".popup");
-    // устанавливаем обработчик закрытия на крестик
-    button.addEventListener("click", () => closePopup(popup));
-  });
 }
 
-export function closePopup(popup, onOverlayClose, onClosePopupEsc) {
+export function closePopup(popup) {
   popup.classList.remove("popup_is-opened");
   popup.removeEventListener("click", onOverlayClose);
   document.removeEventListener("keydown", onClosePopupEsc);
@@ -24,19 +13,21 @@ export function closePopup(popup, onOverlayClose, onClosePopupEsc) {
 export function onClosePopup(evt) {
   const activePopup = evt.target.closest(".popup");
   if (evt.target.classList.contains("popup__close") && activePopup !== null) {
-    closePopup(activePopup, onOverlayClose, onClosePopupEsc);
+    closePopup(activePopup);
   }
 }
 
 export function onClosePopupEsc(evt) {
-  const popup = document.querySelector(".popup_is-opened");
-  if (evt.key === "Escape" && popup !== null) {
-    closePopup(popup, onOverlayClose, onClosePopupEsc);
+  if (evt.key === "Escape") {
+    const popup = document.querySelector(".popup_is-opened");
+    if (popup !== null) {
+      closePopup(popup);
+    }
   }
 }
 
 export function onOverlayClose(evt) {
   if (evt.target.classList.contains("popup_is-opened")) {
-    closePopup(evt.target, onOverlayClose, onClosePopupEsc);
+    closePopup(evt.target);
   }
 }
