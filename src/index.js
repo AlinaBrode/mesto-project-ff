70efc8ff049d0b1d0ace845389c8cb055c86966e
Alina <alinbrode@yandex.ru>
2024-05-01 17:32:47 +0300

    border

--- src/index.js
+++ src/index.js
@@ -22,6 +22,36 @@ const popupImage = document.querySelector(".popup__image");
 const popupCaption = document.querySelector(".popup__caption");
 const placesList = document.querySelector(".places__list");
 
+const popupEditProfileFieldName = formEditProfile.querySelector(
+  ".popup__input_type_name"
+);
+const popupEditProfileFieldNameError = formEditProfile.querySelector(
+  ".popup__input_type_name-error"
+);
+const formEditProfileSubmitButton =
+  formEditProfile.querySelector(".popup__button");
+const popupEditProfileFieldDescription = formEditProfile.querySelector(
+  ".popup__input_type_description"
+);
+const popupEditProfileFieldDescriptionError = formEditProfile.querySelector(
+  ".popup__input_type_description-error"
+);
+
+const popupNewCardFieldName = formAddCard.querySelector(
+  ".popup__input_type_card-name"
+);
+const popupNewCardFieldNameError = formAddCard.querySelector(
+  ".popup__input_type_name-error"
+);
+
+const formNewCardSubmitButton = formAddCard.querySelector(".popup__button");
+const popupNewCardFieldLink = formAddCard.querySelector(
+  ".popup__input_type_url"
+);
+const popupNewCardFieldLinkError = formAddCard.querySelector(
+  ".popup__input_type_url-error"
+);
+
 function viewImage(event) {
   openPopup(popupTypeImage);
 
@@ -48,6 +78,7 @@ function handleAddCard(evt) {
   const cardLink = formAddCard.link.value;
   formAddCard["place-name"].value = "";
   formAddCard.link.value = "";
+  newCardLinkValid();
   const newCard = addCard(
     {
       name: cardTitle,
@@ -68,6 +99,7 @@ formAddCard.addEventListener("submit", handleAddCard);
 function openDialogEditProfile(evt) {
   formEditProfile.name.value = profileTitle.textContent;
   formEditProfile.description.value = profileDescription.textContent;
+  profileEditNameValid();
 
   openPopup(popupTypeEdit);
 }
@@ -89,3 +121,104 @@ closeButtons.forEach((button) => {
   // устанавливаем обработчик закрытия на крестик
   button.addEventListener("click", () => closePopup(popup));
 });
+
+function isValidString(input) {
+  const regex = /^[A-Za-zА-Яа-яёЁ\s\-]+$/;
+  return regex.test(input);
+}
+
+function setError(field, fieldError, btn, errorClass, message) {
+  fieldError.textContent = message;
+  field.classList.add(errorClass);
+  btn.disabled = true;
+}
+
+function clearError(fieldError, btn) {
+  fieldError.textContent = "";
+  btn.disabled = false;
+}
+
+function profileEditFieldValid(field, fieldError, btn, maxLegth) {
+  const name = field.value;
+
+  if (name.length == 0) {
+    setError(
+      field,
+      fieldError,
+      btn,
+      "popup__input-error",
+      "Вы пропустили это поле"
+    );
+  } else if (name.length < 2) {
+    setError(
+      field,
+      fieldError,
+      btn,
+      "popup__input-error",
+      "Минимальное количество символов: 2. Длина текста сейчас: 1 символ."
+    );
+  } else if (name.length > maxLegth) {
+    setError(
+      field,
+      fieldError,
+      btn,
+      "popup__input-error",
+      `Максимальное количество символов: ${maxLegth}. Длина текста в буквах сейчас: ${name.length}.`
+    );
+  } else if (!isValidString(name)) {
+    setError(
+      field,
+      fieldError,
+      btn,
+      "popup__input-error",
+      "Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы"
+    );
+  } else {
+    clearError(fieldError, btn);
+  }
+}
+
+
+function profileEditNameValid() {
+  profileEditFieldValid(
+    popupEditProfileFieldName,
+    popupEditProfileFieldNameError,
+    formEditProfileSubmitButton,
+    20
+  );
+}
+
+function profileEditDescriptionValid() {
+  profileEditFieldValid(
+    popupEditProfileFieldDescription,
+    popupEditProfileFieldDescriptionError,
+    formEditProfileSubmitButton,
+    200
+  );
+}
+
+function newCardNameValid() {
+  profileEditFieldValid(popupNewCardFieldName, popupNewCardFieldNameError, formNewCardSubmitButton, 20);
+}
+
+function newCardLinkValid() {
+  if (popupNewCardFieldLink.validity.typeMismatch) {
+    setError(
+      popupNewCardFieldLink,
+      popupNewCardFieldLinkError,
+      formNewCardSubmitButton,
+      "popup__input-error",
+      `Введите адрес сайта`
+    );
+  } else {
+    clearError(popupNewCardFieldLinkError, formNewCardSubmitButton);
+  }
+}
+
+popupEditProfileFieldName.addEventListener("input", profileEditNameValid);
+popupEditProfileFieldDescription.addEventListener(
+  "input",
+  profileEditDescriptionValid
+);
+popupNewCardFieldName.addEventListener("input", newCardNameValid);
+popupNewCardFieldLink.addEventListener("input", newCardLinkValid);
