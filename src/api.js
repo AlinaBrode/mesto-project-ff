dc1c93f0f567808cda0a7c61150dc635e32d7956
Alina <alinbrode@yandex.ru>
2024-05-05 14:25:23 +0300

    fix: response ok

--- src/api.js
+++ src/api.js
@@ -1,9 +1,13 @@
 const config = {
-  baseUrl: 'https://nomoreparties.co/v1/wff-cohort-12',
+  baseUrl: "https://nomoreparties.co/v1/wff-cohort-12",
   headers: {
-    authorization: '90ad5c9a-5357-4276-be02-5ea1b5321bf2',
-    'Content-Type': 'application/json'
-  }
+    authorization: "90ad5c9a-5357-4276-be02-5ea1b5321bf2",
+    "Content-Type": "application/json",
+  },
+};
+
+function handleResponse(res) {
+  return Promise.reject(`Ошибка: ${res.status}`);
 }
 
 export function deleteCard(cardId, name, description) {
@@ -14,6 +18,13 @@ export function deleteCard(cardId, name, description) {
       name: name,
       about: description,
     }),
-  }).then((data) => data.json());
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+
+    // если ошибка, отклоняем промис
+    return handleResponse(res);
+  });
 }
 
